08cd72f4ad5dbc064a3c4b74ef591e39329ab840
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-03-26 13:40:03 -0400

    fix(ascii-art): fix ascii art stringifier script

--- ascii.py
+++ ascii.py
@@ -4,13 +4,14 @@ import re
 ascii = open("ascii.txt", "r").read()
 am = open("ascii_mangled.txt", "w")
 
-#ascii = re.sub(r"\"", "\\\"", ascii)
-
 stringified = str(ascii)
 
+stringified = stringified.encode('unicode_escape')
+stringified = re.sub(r"\"", "\\\"", stringified) # escape double quotes since the string will be wrapped in double quotes
+stringified = stringified.encode('unicode_escape')
 stringified = re.sub(r"\"", "\\\"", stringified)
-stringified = re.sub(r"\\", "\\\\", stringified)
-stringified = re.sub(r"\\n", "\\\\n", stringified)
+
+stringified = '"' + stringified + '"'
 
 am.write(stringified)
 am.close()
