6877d683969d9e6b2b02056ca74dc692d7f5916e
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-05-14 08:19:39 -0400

    chore(theme-patch): add fgp-int theme to patcher

--- ramp-theme-patch.sh
+++ ramp-theme-patch.sh
@@ -1,5 +1,5 @@
 RAMPDIR='C:\Users\Aleksueir\Code\TFS2013'
-THEMES='canada intranet usability'
+THEMES='canada intranet usability fgp-int'
 THEME=$1
 PATCH=$2
 STARTDIR=`pwd`
@@ -11,7 +11,7 @@ STARTDIR=`pwd`
 #patch command is indicating changes to propagate; can be something like "-1 <sha>" for a single commit patch or "master" for changes from the current against master
 makepatch()
 {
-	git format-patch $PATCH --stdout > $RAMPDIR/p_auto.patch
+    git format-patch $PATCH --stdout > $RAMPDIR/p_auto.patch
 }
 
 patchtheme()
