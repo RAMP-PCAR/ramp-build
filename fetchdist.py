e9fb203698f7591f21a81dc4b54577c7cc6a0c5c
Aly Merchant <aly.merchant@ec.gc.ca>
2015-05-19 18:05:37 -0400

    print links when deploying a new release

--- fetchdist.py
+++ fetchdist.py
@@ -1,44 +1,50 @@
-import requests, shutil, zipfile, os, sys
+from __future__ import print_function
 from StringIO import StringIO
+
+import requests, shutil, zipfile, os, sys, parsever
 
 paths = {
             'core': { 'repo':'ramp-pcar-dist', 'ref':None },
             'canada': { 'repo':'ramp-theme-dist', 'ref':'ramp-theme-canada' },
             'intranet': { 'repo':'ramp-theme-dist', 'ref':'ramp-theme-intranet' },
             'usability': { 'repo':'ramp-theme-dist', 'ref':'ramp-theme-usability' },
+            'fgp-int': { 'repo':'ramp-theme-dist', 'ref':'ramp-theme-fgp-int' },
         }
 
 def fetch_theme( fname, extract_path, auth ):
     j = requests.get( url, auth=auth ).json()
     for asset in j:
-        print asset.get('name','NO NAME')
+        print(asset.get('name','NO NAME'))
         if asset.get('name','NO NAME') == fname:
             break
     else:
-        print 'NOT FOUND'
+        print('NOT FOUND')
         return
 
     dl_url = asset.get('download_url','NO URL')
-    print dl_url
+    print(dl_url)
     a = requests.get(dl_url, auth=auth)
     if a.status_code == 200:
         data = StringIO(a.content)
         with zipfile.ZipFile(data, "r") as z:
             z.extractall( extract_path )
     else:
-        print 'status:', a.status_code
+        print('status:', a.status_code)
 
 if __name__ == '__main__':
 
     if len(sys.argv) < 2:
-        print 'Usage {0} [version]'.format(sys.argv[0])
+        print('Usage {0} [version]'.format(sys.argv[0]))
         sys.exit(1)
     ver = sys.argv[1]
+    if not parsever.match_version(ver):
+        print('Please provide a version in the form A.B.C[-D]')
+        sys.exit(1)
+    tag = 'v'+ver
     tok = None
     with open('.token') as f:
         tok = f.read().strip()
-    print 'Fetching RAMP v{0}'.format(ver)
-    tag = 'v'+ver
+    print('Fetching RAMP v{0}'.format(ver))
     auth = (tok,'x-oauth-basic')
     os.mkdir( tag )
     for theme in paths:
@@ -53,8 +59,12 @@ if __name__ == '__main__':
         url = 'https://api.github.com/repos/RAMP-PCAR/{repo}/contents/tarball?ref={ref}'.format( **paths[theme] )
         fetch_theme( paths[theme]['filename'], paths[theme]['path'], auth )
 
-    print 'Copying to dev'
+    print()
+    print('Copying to dev')
     shutil.copytree( tag, 'x:\\wwwroot\\RAMP_QC\\{0}'.format(tag) )
+    if 'RAMPQCSERVER' in os.environ:
+        for theme in paths:
+            print('http://{0}/RAMP_QC/{1}/{2}/ramp-en.html'.format(os.environ['RAMPQCSERVER'],tag,theme))
 
 #   if a.status_code == 200:
 #       with open(zfile, 'wb') as f:
--- parsever.py
+++ parsever.py
@@ -1,10 +1,13 @@
 import re, sys
 
+def match_version(s):
+    r = re.compile(ur'^(\d+\.\d+\.\d+(?:-\d+)?)', re.MULTILINE)
+    return re.search(r, sys.argv[1])
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         sys.exit(1)
-    r = re.compile(ur'(\d+\.\d+\.\d+(?:-\d+)?)', re.MULTILINE)
-    m = re.search(r, sys.argv[1])
+    m = match_version( sys.argv[1] )
     if m is None:
         sys.exit(1)
     parts = m.group(1).split('-')
