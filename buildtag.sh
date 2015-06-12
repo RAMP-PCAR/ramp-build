4330032894d3d9a175d27b96b8e991d9223d7492
Aly Merchant <aly.merchant@ec.gc.ca>
2015-06-12 12:00:18 -0400

    hard reset to avoid merge states

--- buildtag.sh
+++ buildtag.sh
@@ -7,7 +7,10 @@ VER=$1
 OLDPWD=`pwd`
 
 # undo a build
-# for t in $THEMES; do cd ../ramp-$t; git push github :v5.2.0-rc1; git push origin :v5.2.0-rc1; done
+# cd ramp
+# TAG=v5.2.0-rc1
+# git tag -d $TAG && git push github :$TAG && git push origin :$TAG
+# for t in $THEMES; do cd ../ramp-$t; git tag -d $TAG && git push github :$TAG && git push origin :$TAG; done
 
 if [ -z $BRANCH ]; then
     BRANCH='develop'
@@ -17,6 +20,7 @@ cd $RAMPDIR/ramp
 git fetch
 git co $BRANCH
 git pull
+git reset --hard origin/$BRANCH
 if [ "$VER" == 'auto' ]; then
     VER=`python $OLDPWD/incver.py package.json`
     echo "Auto versioning with $VER"
@@ -50,6 +54,7 @@ for t in $THEMES; do
     git fetch
     git co $BRANCH
     git pull
+    git reset --hard origin/$BRANCH
     sed -i "s/\"ramp-pcar\".*/\"ramp-pcar\":\ \"https:\/\/github.com\/RAMP-PCAR\/RAMP-PCAR.git#$TAG\",/" bower.json
     tag_sync $TAG
 done
