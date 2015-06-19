3f3453925691250afe462b7b404e3c5f284dfc59
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-06-19 07:30:00 -0400

    feat(patch): add branch parameter

--- ramp-theme-patch.sh
+++ ramp-theme-patch.sh
@@ -2,10 +2,11 @@ RAMPDIR='C:\Users\Aleksueir\Code\TFS2013'
 THEMES='canada intranet usability fgp-int'
 THEME=$1
 PATCH=$2
+BRANCH=$3
 STARTDIR=`pwd`
 
 #Usage:
-#ramp-theme-patch [theme-name] [patch command]
+#ramp-theme-patch [theme-name] [patch command] [branch name]
 #
 #theme-name is the name of the theme where the changes are
 #patch command is indicating changes to propagate; can be something like "-1 <sha>" for a single commit patch or "master" for changes from the current against master
@@ -22,12 +23,14 @@ patchtheme()
 rm -f $RAMPDIR/p_auto.patch
 
 cd $RAMPDIR/ramp-theme-$THEME
+git co $BRANCH
 makepatch
 
 for t in $THEMES; do
     cd $RAMPDIR/ramp-theme-$t
 	
 	if [ "$t" != "$THEME" ]; then
+        git co $BRANCH
 		patchtheme
 	fi		
 done
