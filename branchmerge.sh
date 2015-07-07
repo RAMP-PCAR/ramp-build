fd22c282a561061b2af4fa324fda7dcd238668af
Aly Merchant <aly.merchant@ec.gc.ca>
2015-07-07 08:05:19 -0400

    use hard reset to drop local changes

--- branchmerge.sh
+++ branchmerge.sh
@@ -8,9 +8,12 @@ STARTDIR=`pwd`
 
 mergedst()
 {
+    git fetch
     git co $SRC
+    git reset --hard origin/$SRC
     git pull
     git co $DST
+    git reset --hard origin/$DST
     git pull
     git merge $SRC
     git push origin
